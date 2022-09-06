b5a4a5ea83127294e2075ef9ae764a7ca8f201c7
Ap73MKa <artyom.vlasov.05@mail.ru>
2022-09-06 16:18:17 +0300

    add pacman

--- user_bot/handlers/vip/stickers.py
+++ user_bot/handlers/vip/stickers.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler
 
-from user_bot.handlers.vip.util import _get_heart_stickers
+from user_bot.handlers.vip.util import _get_heart_stickers, _get_pacman_stickers
 from user_bot.utils import get_me_filters, play_stroke_anim, cmd
 
 
@@ -43,9 +43,18 @@ async def __heart(app: Client, msg: Message):
         await sleep(0.2)
 
 
+@cmd(False)
+async def __pacman(app: Client, msg: Message):
+    img = _get_pacman_stickers()
+    for anim in img:
+        await msg.edit('\n'.join(anim))
+        await sleep(0.2)
+
+
 def _get_sticker_vip_handlers() -> tuple[MessageHandler, ...]:
     return (
         MessageHandler(__like, filters=get_me_filters('like')),
         MessageHandler(__dislike, filters=get_me_filters('dislike')),
         MessageHandler(__heart, filters=get_me_filters('heart')),
+        MessageHandler(__pacman, filters=get_me_filters('pacman')),
     )
